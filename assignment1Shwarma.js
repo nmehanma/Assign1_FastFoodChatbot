5826c669116d539589c30e3cf58e9fbcd519e8d7
nmehanma <nmehanma@gmail.com>
2022-01-26 23:57:54 -0500

    redo assignment, giving user the option of two items to choose from

--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -1,59 +1,97 @@
 const Order = require("./assignment1Order");
 
 const OrderState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    SIZE:   Symbol("size"),
-    Type:   Symbol("type"),
-    SECOND_ITEM: Symbol("second"),
-    DRINKS:  Symbol("drinks"),
-    Fruit: Symbol("Fruit")
+  WELCOMING: Symbol("welcoming"),
+  SIZE: Symbol("size"),
+  TYPE: Symbol("type"),
+  TYPE2: Symbol("type2"),
+  SECOND_ITEM: Symbol("second"),
+  DRINKS: Symbol("drinks"),
+  FRUITS: Symbol("Fruits")
 });
 
-module.exports = class ShwarmaOrder extends Order{
-    constructor(){
-        super();
-        this.stateCur = OrderState.WELCOMING;
-        this.sSize = "";
-        this.sType = "";
-        this.sDrinks = "";
-        this.sItem = "Seafood";
-        this.sItem2 = "Soup";
-        this.sItem2_type="Mushroom";
-        this.sFruit = " ";
-    }
-    handleInput(sInput){
-        let aReturn = [];
-        switch(this.stateCur){
-            case OrderState.WELCOMING:
-                this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcome to Conestoga's Seafood.");
-                aReturn.push("What meal size would you like?");
-                break;
-            case OrderState.SIZE:
-                this.stateCur = OrderState.Type
-                this.sSize = sInput;
-                aReturn.push("What type would you like?");
-                break;
-            case OrderState.Type:
-                this.stateCur = OrderState.DRINKS
-                this.sType = sInput;
-                aReturn.push("Would you like drinks with that?");
-                break;
-            case OrderState.DRINKS:
-                this.isDone(true);
-                if(sInput.toLowerCase() != "no"){
-                    this.sDrinks = sInput;
-                }
-                aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} meals of ${this.sItem} of type ${this.sType}`);
-                if(this.sDrinks){
-                    aReturn.push(`with a drink of ${this.sDrinks}`);
-                }
-                let d = new Date(); 
-                d.setMinutes(d.getMinutes() + 20);
-                aReturn.push(`Please pick it up at ${d.toTimeString()}`);
-                break;
+module.exports = class ShwarmaOrder extends Order {
+  constructor() {
+    super();
+    this.stateCur = OrderState.WELCOMING;
+    this.sSize = "";
+    this.sType = "";
+    this.sDrinks = "";
+    this.sDrinks2 = "";
+    this.sFruits = "";
+    this.sFruits2 = "";
+    this.sItem = "BBQ";
+    this.sItem2 = "Soup";
+    this.sItem2_type = "Mushroom";
+  }
+  handleInput(sInput) {
+    let aReturn = [];
+    switch (this.stateCur) {
+      case OrderState.WELCOMING:
+        this.stateCur = OrderState.SIZE;
+        aReturn.push("Welcome to Conestoga's BBQ.");
+        aReturn.push("What meal size would you like?");
+        break;
+      case OrderState.SIZE:
+        this.stateCur = OrderState.TYPE;
+        this.sSize = sInput;
+        aReturn.push("What type would you like?");
+        break;
+      case OrderState.TYPE:
+        this.stateCur = OrderState.DRINKS;
+        this.sType = sInput;
+        console.log("1");
+        aReturn.push("Would you like a drink with that? If yes please specify");
+        break;
+      case OrderState.DRINKS:
+        this.stateCur = OrderState.FRUITS;
+        this.sDrinks = sInput;
+        console.log("2");
+        aReturn.push("Would you like a fruit with that? If yes please specify");
+        break;
+      case OrderState.FRUITS:
+        this.stateCur = OrderState.TYPE2;
+        this.sFruits = sInput;
+        console.log("3");
+        aReturn.push(
+          "Would you like a second item as well? If yes please specify"
+        );
+        break;
+      case OrderState.TYPE2:
+        this.stateCur = OrderState.DRINKS2;
+        this.sType2 = sInput;
+        console.log("4");
+        aReturn.push("Would you like a drink with that? If yes please specify");
+        break;
+      case OrderState.DRINKS2:
+        this.stateCur = OrderState.FRUITS2;
+        this.sDrinks2 = sInput;
+        console.log("5");
+        aReturn.push("Would you like a fruit with that? If yes please specify");
+        break;
+      case OrderState.FRUITS2:
+        if (sInput.toLowerCase() != "no") {
+          this.sFruits2 = sInput;
         }
-        return aReturn;
+        aReturn.push("Thank-you for your order of");
+        aReturn.push(
+          `${this.sSize} meal of ${this.sItem} of type ${this.sType}`
+        );
+        if (this.sDrinks) {
+          aReturn.push(`with a drink of ${this.sDrinks}`);
+        }
+        if (this.sFruits) {
+          aReturn.push(`with a side of ${this.sFruits}`);
+        }
+        if (this.sType2) {
+          aReturn.push(`and a second order of ${this.sType2} meal of ${this.sItem2} of type ${this.sType}`);
+        }
+
+        let d = new Date();
+        d.setMinutes(d.getMinutes() + 20);
+        aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+        break;
     }
-}
+    return aReturn;
+  }
+};
