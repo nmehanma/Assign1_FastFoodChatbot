4d81ce72033d75df8bc7f69f0055045c072e67de
nmehanma <nmehanma@gmail.com>
2022-01-27 00:57:46 -0500

    cleaned up code to allow proper questioning in sequential order

--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -7,7 +7,9 @@ const OrderState = Object.freeze({
   TYPE2: Symbol("type2"),
   SECOND_ITEM: Symbol("second"),
   DRINKS: Symbol("drinks"),
-  FRUITS: Symbol("Fruits")
+  DRINKS2: Symbol("drinks"),
+  FRUITS: Symbol("Fruits"),
+  FRUITS2: Symbol("Fruits")
 });
 
 module.exports = class ShwarmaOrder extends Order {
@@ -16,11 +18,12 @@ module.exports = class ShwarmaOrder extends Order {
     this.stateCur = OrderState.WELCOMING;
     this.sSize = "";
     this.sType = "";
+    this.sType2 = "";
     this.sDrinks = "";
     this.sDrinks2 = "";
     this.sFruits = "";
     this.sFruits2 = "";
-    this.sItem = "BBQ";
+    this.sItem = "country BBQ";
     this.sItem2 = "Soup";
     this.sItem2_type = "Mushroom";
   }
@@ -45,13 +48,17 @@ module.exports = class ShwarmaOrder extends Order {
         break;
       case OrderState.DRINKS:
         this.stateCur = OrderState.FRUITS;
-        this.sDrinks = sInput;
+        if (sInput.toLowerCase() != "no") {
+          this.sDrinks = sInput;
+        }
         console.log("2");
         aReturn.push("Would you like a fruit with that? If yes please specify");
         break;
       case OrderState.FRUITS:
         this.stateCur = OrderState.TYPE2;
-        this.sFruits = sInput;
+        if (sInput.toLowerCase() != "no") {
+          this.sFruits = sInput;
+        }
         console.log("3");
         aReturn.push(
           "Would you like a second item as well? If yes please specify"
@@ -59,17 +66,40 @@ module.exports = class ShwarmaOrder extends Order {
         break;
       case OrderState.TYPE2:
         this.stateCur = OrderState.DRINKS2;
+        if (sInput.toLowerCase() != "no") {
+          this.type2 = sInput;
+        } else {
+          this.isDone(true);
+          aReturn.push("Thank-you for your order of");
+          aReturn.push(
+            `${this.sSize} meal of ${this.sItem} of type ${this.sType}`
+          );
+          if (this.sDrinks) {
+            aReturn.push(`with a drink of ${this.sDrinks}`);
+          }
+          if (this.sFruits) {
+            aReturn.push(`with a side of ${this.sFruits}`);
+          }
+          let d = new Date();
+          d.setMinutes(d.getMinutes() + 20);
+          aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+          break;
+        }
         this.sType2 = sInput;
         console.log("4");
         aReturn.push("Would you like a drink with that? If yes please specify");
         break;
       case OrderState.DRINKS2:
         this.stateCur = OrderState.FRUITS2;
-        this.sDrinks2 = sInput;
+        if (sInput.toLowerCase() != "no") {
+          this.sDrinks2 = sInput;
+        }
         console.log("5");
         aReturn.push("Would you like a fruit with that? If yes please specify");
         break;
       case OrderState.FRUITS2:
+        this.isDone(true);
+        console.log("here");
         if (sInput.toLowerCase() != "no") {
           this.sFruits2 = sInput;
         }
@@ -84,7 +114,15 @@ module.exports = class ShwarmaOrder extends Order {
           aReturn.push(`with a side of ${this.sFruits}`);
         }
         if (this.sType2) {
-          aReturn.push(`and a second order of ${this.sType2} meal of ${this.sItem2} of type ${this.sType}`);
+          aReturn.push(
+            `and a second order of a ${this.sSize} meal of ${this.sItem} of type ${this.sType2}`
+          );
+        }
+        if (this.sDrinks2) {
+          aReturn.push(`with a drink of ${this.sDrinks2}`);
+        }
+        if (this.sFruits2) {
+          aReturn.push(`with a side of ${this.sFruits2}`);
         }
 
         let d = new Date();
